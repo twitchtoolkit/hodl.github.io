992e36c9343557de686c5f2de6b237fec83513db
spoonerton <spoonerton@gmail.com>
2021-04-02 11:09:40 -0400

    Update customJS.js

--- customJS.js
+++ customJS.js
@@ -1,4 +1,2 @@
-<script>    
 const itemListURL = "https://raw.githubusercontent.com/spoonerton/Spoons-Shop/master/StoreItems.json";
 const eventListURL = "https://raw.githubusercontent.com/spoonerton/Spoons-Shop/master/StoreIncidents.json";
-</script>
